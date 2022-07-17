31fa46006e93cf5f20f59e2f1d75f0fe8ace3880
kevin <skullspine9@gmail.com>
2022-07-18 01:33:49 +0800

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# kevinchua6.github.io
-
-My portfolio website repository. This is the codebase behind my website at [https://kevinchua6.github.io/](https://kevinchua6.github.io/).
---
+++ precache-manifest.800fd44faee10e98e16c0af3d4213e5d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1a589967b17243841788326af7c433",
+    "url": "/index.html"
+  },
+  {
+    "revision": "622af504fbe149f9495c",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "321dbeb7cbd1351df26b",
+    "url": "/static/css/main.29dcba89.chunk.css"
+  },
+  {
+    "revision": "622af504fbe149f9495c",
+    "url": "/static/js/2.9976322d.chunk.js"
+  },
+  {
+    "revision": "bcce567bfd84a848f2f927da6896db7a",
+    "url": "/static/js/2.9976322d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "321dbeb7cbd1351df26b",
+    "url": "/static/js/main.dccff5c4.chunk.js"
+  },
+  {
+    "revision": "504732cfaf834efe7bdd",
+    "url": "/static/js/runtime-main.146895b5.js"
+  },
+  {
+    "revision": "b7d3cc02821bc4f4efc31197145e0f51",
+    "url": "/static/media/2018-ecg-fair.b7d3cc02.md"
+  },
+  {
+    "revision": "86cb8f9b09478901c54e991fccde2f78",
+    "url": "/static/media/2019-garden-campus.86cb8f9b.md"
+  },
+  {
+    "revision": "b7b45e92041cf207a970f79cd44c8544",
+    "url": "/static/media/2019-open-house.b7b45e92.md"
+  },
+  {
+    "revision": "10aacce61bf393bff2b65da0205237e1",
+    "url": "/static/media/lifexp-bot.10aacce6.md"
+  },
+  {
+    "revision": "b4306989476cfc87fc12c917471c9ffa",
+    "url": "/static/media/mindful-hacks.b4306989.jpg"
+  },
+  {
+    "revision": "d5e74e521d4b859f616444b239be4cfb",
+    "url": "/static/media/timetable-bot.d5e74e52.md"
+  }
+]);
